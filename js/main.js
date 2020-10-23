0f8a590281c10bec8c900f77bd202f1b98f534fb
cristianfernandoromero1 <cfromero88@gmail.com>
2020-10-23 18:32:51 -0500

    agregue archivos de otro proyecto

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var semanaSeleccionada = 0;
+
+var app = new Vue({
+    el: '#app',
+    data: {
+
+    },
+    
+    methods: {
+    operacion: function() {
+      //Promedio de programación
+      if(semanaSeleccionada == 1)
+      {
+
+      }
+     
+    },
+    
+  }
+  })
+
+
+
+  function subBoton1() 
+  {
+      //window.location.href = "http://www.google.com";
+      alert("Semana 1 seleccionada");
+      semanaSeleccionada = 1;
+  }
+
+  function subBoton2() 
+  {
+      //window.location.href = "http://www.google.com";
+      alert("Semana 2 seleccionada");
+      semanaSeleccionada = 2;
+  }
+
+  function subBoton3() 
+  {
+      //window.location.href = "http://www.google.com";
+      alert("Semana 3 seleccionada");
+      semanaSeleccionada = 3;
+  }
+
+  function subBoton4() 
+  {
+      //window.location.href = "http://www.google.com";
+      alert("Semana 4 seleccionada");
+      semanaSeleccionada = 4;  
+  }
